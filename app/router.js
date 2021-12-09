b1a0fcd64db51175ce72d8be52e278907fb6b497
letuankhoi.dev <letuankhoi.dev@gmail.com>
2021-12-09 15:40:44 +0700

    core: init compont

--- app/router.js
+++ app/router.js
@@ -8,4 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('home');
+  this.route('about', { path: '/more-info' });
+  this.route('login', { path: '/authenticated' });
 });
